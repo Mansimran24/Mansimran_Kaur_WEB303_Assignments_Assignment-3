b0a60994f1e437c4534ed6f1690e6dddf8fe0381
Mansimran Kaur <91445014+Mansimran24@users.noreply.github.com>
2022-06-11 17:43:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
 // WEB303 Assignment 3
-// Your Name Here
+// Mansimran Kaur
+let glitchTeam = $.getJSON("glitchTeam.json").done(function(data){
+    $.each(data.members,function(a,b){
+        console.log("data a:",a,"and data b:",b);
+  
+        $('#glitchTeam').append(`
+        <h2>${b.name}</h2>
+        <h5>${b.position}</h5>
+        <p>${b.bio}</p>
+        `);
+    })  ;
+  
+  });
+  console.log("the tglitchTeam :", glitchTeam);
+  
+  $(function usingAjax() {
+      $.ajax({url: "glitchTeam.json", 
+      beforeSend: function() {
+          let loadText = $("<h2></h2>").text("Loading...");
+          $(`div#glitchTeam`).append(loadText);
+      },
+      error: function(){
+          
+      },
+      timeout: 5000,
+  
+      
+      success: function(data){
+          $(`div#glitchTeam`).empty();
+          $.each(data.members,function(a,b){
+              console.log("data a:",a,"and data b:",b);
+        
+              $('#glitchTeam').append(`
+              <h2>${b.name}</h2>
+              <h5>${b.position}</h5>
+              <p>${b.bio}</p>
+              `);
+          })  
+      }
+  });
+  });
+  
+  
